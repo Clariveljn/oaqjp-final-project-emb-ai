528d2ddacee3be618d4c480125598f5ddb49f380
Clariveljn <cjeldres5@gmail.com>
2026-06-10 20:27:15 -0400

    feat: add blank input error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -28,6 +28,16 @@ def emotion_detector(text_to_analyse):
         headers=headers
     )
 
+    if response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
+
     formatted_response = response.json()
 
     emotions = formatted_response["emotionPredictions"][0]["emotion"]
